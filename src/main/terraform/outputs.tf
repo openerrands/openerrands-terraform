fcab0750dd8481577fb27a4bf1f513410a14ff0d
Scott Severtson <scott@severtson.us>
2020-11-10 22:03:55 -0500

    More S3 metadata

--- src/main/terraform/outputs.tf
+++ src/main/terraform/outputs.tf
@@ -1,11 +1,31 @@
 output "s3_web_arn" {
   value = aws_s3_bucket.web.arn
 }
-output "s3_app_cdn" {
+output "s3_web_bucket" {
+  value = aws_s3_bucket.web.bucket
+}
+output "s3_web_endpoint" {
+  value = aws_s3_bucket.web.website_endpoint
+}
+
+output "s3_cdn_arn" {
   value = aws_s3_bucket.cdn.arn
 }
+output "s3_cdn_bucket" {
+  value = aws_s3_bucket.cdn.bucket
+}
+output "s3_cdn_endpoint" {
+  value = aws_s3_bucket.cdn.website_endpoint
+}
+
 output "s3_app_arn" {
   value = aws_s3_bucket.app.arn
+}
+output "s3_app_bucket" {
+  value = aws_s3_bucket.app.bucket
+}
+output "s3_app_endpoint" {
+  value = aws_s3_bucket.app.website_endpoint
 }
 
 output "api_gateway_arn" {
